c8bf02f6508496e3dbdeccff15974df32e1d36da
carlo <carlo@tec2.volta.fano>
2014-02-05 13:16:56 +0100

    primo commit prima di andare a pranzo

--- report.php
+++ report.php
@@ -3,6 +3,30 @@
   <div class="report">
   </div>
   <div id="contenuto">
-    ciao
+    <form action="report.php" method="post">
+      <table>
+        <tr class="riga1">
+          <td>Seleziona il nome:</td>
+          <td>
+            <select class="bottone" name="filtro">
+              <option value="tutti">Tutti</option>
+            </select>
+          </td>
+          <td>
+            <input class="bottone" type="submit" value="filtra" />
+          </td>
+      </table>
+    </form>
+    <table>
+      <tr>
+        <th>Computer</th>
+        <th>Ubicazione</th>
+        <th>Problema riscontrato</th>
+        <th>Soluzione</th>
+        <th>Data Apertura</th>
+        <th>Data Chiusuma</th>
+        <th>Nome</th>
+      </tr>
+    </table>
   </div>
 <?php include("footer.php"); ?>
